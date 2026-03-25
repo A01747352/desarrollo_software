a67af0ff59760979c2c9be4d88f1fdf63023bb5f
Diego Carreon <A01747352@tec.mx>
2026-03-25 16:33:28 -0600

    c2.setup

---
+++ covid-19/src/App.tsx
@@ -0,0 +1,13 @@
+import Titulo from './components/Titulo'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Titulo />
+    </>
+  )
+}
+
+export default App
---
+++ covid-19/src/components/Titulo.tsx
@@ -0,0 +1,7 @@
+const Titulo = () => {
+    return (
+        <h1>Covid-19</h1>
+    );
+}
+
+export default Titulo;
