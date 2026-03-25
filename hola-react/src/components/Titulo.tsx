6407dae4b4fc32664b47c91b992bf3b004de0376
Diego Carreon <A01747352@tec.mx>
2026-03-24 18:22:33 -0600

    Test1

---
+++ hola-react/src/components/Titulo.tsx
@@ -0,0 +1,11 @@
+const Titulo = () => {
+  return (
+    <>
+        <h1 className="titulo">¡Hola, React!</h1>
+        <h2 className="subtitulo">Aprendiendo React</h2>
+    </>
+
+  );
+}
+
+export default Titulo;
